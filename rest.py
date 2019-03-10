8a08fb18ea0a77db0a2018ccfa55dee06c8cf21c
Annmayn <ntvirus333@gmail.com>
2019-03-10 19:11:36 +0545

    added image return in get request

--- rest.py
+++ rest.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ api = Api(app)
 def indexPage():
 	return "Front Page"
 
+#handle site.com/<id>
 class ToDoSimple(Resource):
 	def get(self, todo_id):
 		return 'hello'
@@ -16,7 +17,13 @@ class ToDoSimple(Resource):
 	def put(self, todo_id):
 		pass
 
+#handle site.com/image/id
+class ImageHandler(Resource):
+	def get(self, imageId):
+		return send_file("images/img.jpg", mimetype='image/gif')
+
 api.add_resource(ToDoSimple, '/<todo_id>')
+api.add_resource(ImageHandler, '/image/<imageId>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
